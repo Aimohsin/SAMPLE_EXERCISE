ba892bc90a7388c87d164c65953e4f67c036ac2f
aimohsin03 <aimanmohsin03@gmail.com>
2020-09-04 15:58:18 +0500

    Commit

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import WelcomeScreen from './ap/screen/WelcomeScreen';
+import ViewImageScreen from './ap/screen/ViewImageScreen';
+import Main from './component/MainComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
+     //<ViewImageScreen />
+  // <WelcomeScreen />
   );
 }
 
@@ -15,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems:'center'
   },
 });
